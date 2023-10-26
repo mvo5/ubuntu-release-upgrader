209b259baab1f318aa6d8f2b850d10697e275c2e
Nick Rosbrook <enr0n@ubuntu.com>
2023-10-26 14:56:49 -0400

    Update for noble

--- DistUpgrade/DistUpgradeQuirks.py
+++ DistUpgrade/DistUpgradeQuirks.py
@@ -114,10 +114,10 @@ class DistUpgradeQuirks(object):
         logging.debug("running Quirks.PreCacheOpen")
 
     # individual quirks handler that run *after* the cache is opened
-    def manticPostInitialUpdate(self):
+    def noblePostInitialUpdate(self):
         # PreCacheOpen would be better but controller.abort fails terribly
         """ run after the apt cache is opened the first time """
-        logging.debug("running Quirks.manticPostInitialUpdate")
+        logging.debug("running Quirks.noblePostInitialUpdate")
         self._get_from_and_to_version()
         self._test_and_fail_on_i386()
         self._test_and_fail_on_aufs()
@@ -141,8 +141,8 @@ class DistUpgradeQuirks(object):
         if self._snapstore_reachable:
             self._calculateSnapSizeRequirements()
 
-    def manticPostUpgrade(self):
-        logging.debug("running Quirks.manticPostUpgrade")
+    def noblePostUpgrade(self):
+        logging.debug("running Quirks.noblePostUpgrade")
         cache = self.controller.cache
         if 'snapd' not in cache:
             logging.debug("package required for Quirk not in cache")
